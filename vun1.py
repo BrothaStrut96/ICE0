bb85945a9d916be626b3d1c780f16a644e2e6295
BrothaStrut96 <32519541+BrothaStrut96@users.noreply.github.com>
2019-02-05 22:28:00 -0500

    v1update
    Removed Errors

--- vun1.py
+++ vun1.py
@@ -14,24 +14,15 @@ def long_function_name(
 foo = long_function_name(
                          var_one, var_two,
                          var_three, var_four)
-
-
-# Arguments on first line forbidden when not using vertical alignment.
-foo = long_function_name(var_one, var_two,
-                         var_three, var_four)
-
-# Further indentation required as indentation is not distinguishable.
-def long_function_name(
-                       var_one, var_two, var_three,
-                       var_four):
-    print(var_one)
-
  
 #  Problem 3 (Located: https://www.python.org/dev/peps/pep-0008/#maximum-line-length)
 
 # No: operators sit far away from their operands
-income = (gross_wages +
-          taxable_interest +
-          (dividends - qualified_dividends) -
-          ira_deduction -
-          student_loan_interest)
+
+#Formatted so operations are clear to the user.
+income = (gross_wages 
+          + taxable_interest 
+          +(dividends 
+          - qualified_dividends) 
+          - ira_deduction 
+          - student_loan_interest)
